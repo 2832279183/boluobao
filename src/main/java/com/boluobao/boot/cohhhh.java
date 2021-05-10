2b7f3534844d86abe505cfb098ee5b5584e4f16b
菠萝包 <2832279183@qq.com>
2021-05-10 15:39:20 +0800

    boluobao

---
+++ src/main/java/com/boluobao/boot/cohhhh.java
@@ -0,0 +1,22 @@
+package com.boluobao.boot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Package: com.boluobao.boot
+ * @Description:
+ * @Author JC
+ * @Create 2021年05月07日 17时41分10秒
+ */
+@RestController
+public class cohhhh {
+
+
+    @RequestMapping("/HHHH")
+    public String hhh01(){
+        return "jhhh";
+    }
+}
---
+++ src/main/java/com/boluobao/boot/manapp.java
@@ -0,0 +1,20 @@
+package com.boluobao.boot;
+
+import jdk.nashorn.internal.objects.annotations.SpecializedFunction;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Package: com.boluobao.boot
+ * @Description:
+ * @Author JC
+ * @Create 2021年05月07日 17时38分04秒
+ */
+
+@SpringBootApplication
+public class manapp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(manapp.class,args);
+    }
+}
